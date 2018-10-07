c8201765b8327cb8eabb4e57fd2d78d558463d72
Sergey Dzyuban <sergey.d@sbtech.com>
2018-10-07 18:33:29 +0300

    time format amd name for ES

--- model/Stats.go
+++ model/Stats.go
@@ -1,7 +1,7 @@
 package model
 
 type Metrics struct {
-	Timestamp string `json:"timespamp"`
+	Timestamp string `json:"@timestamp"`
 	Container string `json:"container"`
 	CPU       string `json:"cpu"`
 	Memory    string `json:"memory"`
